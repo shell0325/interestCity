80c73983cfc143fed0ee6088935a41b0ee16ec3d
菅陽向 <syun@MacBook-Pro.local>
2022-05-15 02:15:04 +0900

    ユーザー登録画面作成

--- front/pages/register/index.vue
+++ front/pages/register/index.vue
@@ -1,8 +1,16 @@
 <template>
   <v-app>
-    <v-form v-model="valid" class="form">
+    <v-form ref="form" v-model="valid" class="form">
       <v-container>
         <v-row class="row">
+          <v-col>
+            <v-text-field
+              v-model="username"
+              :rules="[usernameRules.required, usernameRules.max]"
+              label="UserName"
+              required
+            ></v-text-field>
+          </v-col>
           <v-col>
             <v-text-field
               v-model="email"
@@ -28,9 +36,37 @@
               @click:append="show1 = !show1"
             ></v-text-field>
           </v-col>
+          <v-col>
+            <v-text-field
+              v-model="repassword"
+              label="RePassword"
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[
+                passwordRules.required,
+                passwordRules.min,
+                passwordRules.max,
+              ]"
+              :type="show2 ? 'text' : 'password'"
+              name="input-10-1"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-col>
         </v-row>
-        <v-btn>ログイン</v-btn>
-        <v-btn @click="register()">会員登録</v-btn>
+        <v-btn
+          color="primary"
+          dark
+          :disabled="
+            password !== repassword ||
+            password === '' ||
+            email === '' ||
+            username === ''
+          "
+          class="text-caption addChannel mr-0 px-0"
+        >
+          ユーザー登録
+        </v-btn>
+
+        <v-btn @click="login()">ログイン画面へ</v-btn>
       </v-container>
     </v-form>
   </v-app>
@@ -40,23 +76,30 @@
 import { validationMixin } from 'vuelidate'
 import { required, email } from 'vuelidate/lib/validators'
 export default {
-  name: 'LoginPage',
+  name: 'RegisterPage',
+  auth: false,
   mixins: [validationMixin],
   validations: {
     email: { required, email },
   },
-
   data() {
     return {
+      username: '',
       email: '',
       password: '',
+      repassword: '',
       valid: false,
+      usernameRules: {
+        required: (value) => !!value || '入力してください',
+        max: (value) => value.length <= 20 || '20文字以下で入力してください',
+      },
       passwordRules: {
         required: (value) => !!value || '入力してください',
-        min: (v) => v.length >= 8 || '８文字以上入力してください',
-        max: (v) => v.length <= 20 || '20文字以下で入力してください',
+        min: (value) => value.length >= 8 || '８文字以上入力してください',
+        max: (value) => value.length <= 20 || '20文字以下で入力してください',
       },
       show1: false,
+      show2: false,
     }
   },
   computed: {
@@ -68,13 +111,10 @@ export default {
       return errors
     },
   },
-  mounted() {
-    this.email = ''
-    this.password = ''
-  },
+
   methods: {
-    register() {
-      this.$router.push('/register')
+    login() {
+      this.$router.push('/login')
     },
   },
 }
@@ -89,7 +129,6 @@ export default {
   width: 50%;
   margin-inline: auto;
 }
-
 .row {
   display: block;
 }
